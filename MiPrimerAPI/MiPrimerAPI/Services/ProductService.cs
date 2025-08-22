469ea4ef3111dcf75ce8953fed3cedf25e8da566
Rusbert-Carpio <carpiorrr2005@gmail.com>
2025-08-22 17:58:02 -0400

    Add files via upload

---
+++ MiPrimerAPI/MiPrimerAPI/Services/ProductService.cs
@@ -0,0 +1,9 @@
+﻿using MiPrimerAPI.Models;
+
+public class ProductService
+{
+    private readonly IProductRepository _repo;
+    public ProductService(IProductRepository repo) => _repo = repo;
+
+    public Task<IEnumerable<Product>> GetAllProducts() => _repo.GetAllAsync();
+}
